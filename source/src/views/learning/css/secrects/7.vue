3336bb5d77dca072817a0779643bc9e69bbf497a
zhangjialin <jialin2013@foxmail>
2017-01-17 23:12:37 +0800

    add new content

--- source/src/views/learning/css/secrects/7.vue
+++ source/src/views/learning/css/secrects/7.vue
@@ -14,7 +14,14 @@
   text-align: center;
   margin: 0 auto;
 }</code></pre>
-      <h3 class="title"></h3>
+      <h3 class="title">精确控制表格列宽</h3>
+      <p>表格的列宽通常都是根据内容进行调整的，即使显示地指定了width。CSS2.1有一个属性称之为：<strong>table-layout</strong>，默认值为auto，此时浏览器会使用自动表格布局算法调整表格的布局。如果设置为另外的一个值：fixed，则可以对表格更多控制。</p>
+      <p>这种固定表格布局算法将使表格布局更快。应用此属性时需要同时指定表格的宽度。</p>
+      <h3 class="title">根据兄弟元素的数量来设置样式</h3>
+      <p>只有一个列表项时，可以使用:only-child，它等效于:first-child:last-child。即第一项同时也是最后一项。</p>
+      <p>例如ul元素下的li列表项，当列表项有4项时，以下选择符可以命中所有项：</p>
+      <pre><code>li:first-child:nth-last-child(4),li:first-child:nth-last-child(4) ~ li { }</code></pre>
+
     </div>
     <footer>2017年01月10日</footer>
   </div>
