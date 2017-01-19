2521d539c22e400633c34f4f803833b429073e33
zhangjialin <jialin2013@foxmail>
2017-01-19 23:24:10 +0800

    add buju

--- source/src/views/learning/css/secrects/7.vue
+++ source/src/views/learning/css/secrects/7.vue
@@ -21,6 +21,47 @@
       <p>只有一个列表项时，可以使用:only-child，它等效于:first-child:last-child。即第一项同时也是最后一项。</p>
       <p>例如ul元素下的li列表项，当列表项有4项时，以下选择符可以命中所有项：</p>
       <pre><code>li:first-child:nth-last-child(4),li:first-child:nth-last-child(4) ~ li { }</code></pre>
+      <p><strong>根据兄弟元素的数量范围来匹配元素：</strong></p>
+      <p>:nth-child()选择符参数可以不仅可以是简单的数字，也可以如an+b类似的表达式，例如<strong>n+b</strong>，可以选中从第b个开始的所有子元素。</p>
+      <pre><code>//当列表项至少包含4项时，命中所有列表项
+li:first-child:nth-last-child(n+4),li:first-child:nth-last-child(n+4) ~ li {}</code></pre>
+      <p>同理，-n+b这种形式的表达式可以选中开头的b个元素。</p>
+      <pre><code>//当列表项最多包含4项时，命中所有列表项
+li:first-child:nth-last-child(-n+4),li:first-child:nth-last-child(-n+4) ~ li {}</code></pre>
+      <h3 class="title">满幅的背景，定宽的内容</h3>
+      <p>书中将如下dom结构：</p>
+      <pre><code>&lt;footer&gt;&lt;div class='wrapper'&gt;&lt;/div&gt;&lt;/footer&gt;</code></pre>
+      <p>样式写法：</p>
+      <pre><code>footer{
+    background: #333;
+  }
+  .wrapper{
+    max-width: 900px;
+    margin: 1em auto;
+  }</code></pre>
+      <p>利用calc，精简成如下的形式：</p>
+      <pre><code>footer{
+    padding: 1em calc(50% - 450px);
+    background: #333;
+  }</code></pre>
+      <h3 class="title">垂直居中</h3>
+      <p><strong>基于绝对定位的方案：</strong>此方案要求元素具有固定的宽度和高度。</p>
+      <pre><code>main {
+  position: absolute;
+  width: 18em;
+  height: 6em;
+  top: 50%;
+  left: 50%;
+  margin-top: -3em;
+  margin-left: -9em;
+}</code></pre>
+      <p>改进型方案（并不要求元素的宽度高度固定）：</p>
+      <pre><code>main {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}</code></pre>
 
     </div>
     <footer>2017年01月10日</footer>
