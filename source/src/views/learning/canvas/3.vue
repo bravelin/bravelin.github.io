717317fd7360b79f208fdf9032f3895915ce09c6
zhangjialin <jialin2013@qq.com>
2017-08-02 09:53:53 +0800

    增加canvas文本内容

--- source/src/views/learning/canvas/3.vue
+++ source/src/views/learning/canvas/3.vue
@@ -5,8 +5,8 @@
         justify-content: center;
     }
     canvas{
-        width: 250px;
-        height: 250px !important;
+        width: 300px;
+        height: 150px !important;
         border: 1px solid #e0e0e0;
         border-radius: 4px;
     }
@@ -27,6 +27,54 @@
                 <li><strong>font</strong>、<strong>textAlign</strong>、<strong>textBaseline</strong></li>
             </ul>
             <h3 class="title">文本的描边与填充</h3>
+            <p>文本的描边（带阴影）：</p>
+            <div class="exp">
+                <canvas ref="c1" width="300" height="150"></canvas>
+            </div>
+            <pre><code>drawStrokedText () {
+    let that = this
+    let el = that.$refs.c1
+    let context = el.getContext('2d')
+    let w = el.width
+    let h = el.height
+    context.textAlign = 'center'
+    context.textBaseline = 'middle'
+    context.strokeStyle = '#f96'
+    context.font = '80px 宋体'
+
+    // 增加阴影
+    context.shadowColor = 'rgba(0,0,0,0.6)'
+    context.shadowOffsetX = 5
+    context.shadowOffsetY = 5
+    context.shadowBlur = 8
+
+    context.strokeText('HTML5', w / 2, h / 2)
+}</code></pre>
+            <p>文本的填充（带阴影）：</p>
+            <div class="exp">
+                <canvas ref="c2" width="300" height="150"></canvas>
+            </div>
+            <pre><code>context.fillStyle = '#f96'
+context.fillText('HTML5', w / 2, h / 2)</code></pre>
+            <p>填充和描边：</p>
+            <div class="exp">
+                <canvas ref="c3" width="300" height="150"></canvas>
+            </div>
+            <pre><code>context.strokeStyle = '#f96'
+context.fillStyle = '#f96'
+context.strokeText('HTML5', w / 2, h / 2)
+context.fillText('HTML5', w / 2, h / 2)</code></pre>
+            <p><strong>strokeText()和fillText()方法可以通过可选的第四参数来指定文本的最大宽度。</strong></p>
+            <h3 class="title">字型属性font设置</h3>
+            <p>font的各个分量：</p>
+            <ul>
+                <li><strong>font-style</strong>：可取值normal、italic（斜体字）、oblique（倾斜的字）</li>
+                <li><strong>font-variant</strong>：可取值normal、small-caps（设置小型大写字母显示，所有小写字母均会被转换为大写，但是字体会显的略小一点）</li>
+                <li><strong>font-weight</strong>：可取值normal、bold、bolder、lighter、100、200、300...900（normal相当于数值400，bold相当于数值700）</li>
+                <li><strong>font-size</strong>：可取值xx-small、x-small、medium、large、x-large、xx-large、smaller、larger、length与%</li>
+                <li><strong>line-height</strong>：浏览器强制设置为normal</li>
+                <li><strong>font-family</strong>：字体名称</li>
+            </ul>
         </div>
         <footer>2016年06月25日</footer>
         <comments></comments>
@@ -37,8 +85,73 @@
     module.exports = {
         extends: Page,
         mounted () {
+            let that = this
+            that.$nextTick(() => {
+                that.drawStrokedText()
+                that.drawFilledText()
+                that.drawStrokedFilledText()
+            })
         },
         methods: {
+            drawStrokedText () {
+                let that = this
+                let el = that.$refs.c1
+                let context = el.getContext('2d')
+                let w = el.width
+                let h = el.height
+                context.textAlign = 'center'
+                context.textBaseline = 'middle'
+                context.strokeStyle = '#f96'
+                context.font = '80px 宋体'
+
+                // 增加阴影
+                context.shadowColor = 'rgba(0,0,0,0.6)'
+                context.shadowOffsetX = 5
+                context.shadowOffsetY = 5
+                context.shadowBlur = 8
+
+                context.strokeText('HTML5', w / 2, h / 2)
+            },
+            drawFilledText () {
+                let that = this
+                let el = that.$refs.c2
+                let context = el.getContext('2d')
+                let w = el.width
+                let h = el.height
+                context.textAlign = 'center'
+                context.textBaseline = 'middle'
+                context.fillStyle = '#f96'
+                context.font = '80px 宋体'
+
+                // 增加阴影
+                context.shadowColor = 'rgba(0,0,0,0.6)'
+                context.shadowOffsetX = 5
+                context.shadowOffsetY = 5
+                context.shadowBlur = 8
+
+                context.fillText('HTML5', w / 2, h / 2)
+            },
+            drawStrokedFilledText () {
+                let that = this
+                let el = that.$refs.c3
+                let context = el.getContext('2d')
+                let w = el.width
+                let h = el.height
+                context.textAlign = 'center'
+                context.textBaseline = 'middle'
+                context.fillStyle = '#f96'
+                context.strokeStyle = '#f96'
+                context.font = '80px 宋体'
+
+                // 增加阴影
+                context.shadowColor = 'rgba(0,0,0,0.6)'
+                context.shadowOffsetX = 5
+                context.shadowOffsetY = 5
+                context.shadowBlur = 8
+
+                context.strokeText('HTML5', w / 2, h / 2)
+                context.fillText('HTML5', w / 2, h / 2)
+            }
         }
     }
 </script>
