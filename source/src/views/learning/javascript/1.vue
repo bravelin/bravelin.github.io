a4c1877dacd49c6585a8a831c29c1c53336a08c8
zhangjialin <jialin2013@qq.com>
2018-05-20 23:06:34 +0800

    增加page

---
+++ source/src/views/learning/javascript/1.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="page article">
+        <h2 class="title">你不知道的JavaScript（上卷）笔记</h2>
+        <div class="content">
+            <h3 class="title">let命令</h3>
+            <p>用于声明变量，作用类似var，但只在let变量的代码块内有效。</p>
+        </div>
+        <footer>2018年05月20日</footer>
+        <comments></comments>
+        <Catalog :catalog="catalog"></Catalog>
+    </div>
+</template>
+<script>
+    import Page from '../../page'
+    export default {
+        extends: Page
+    }
+</script>
--- source/src/views/sentences.vue
+++ source/src/views/sentences.vue
@@ -5,6 +5,7 @@
             <li v-for="item in list" :key="item.id">
                 <h4>{{ item.updatedAt | TimeFilter }}</h4>
                 <h3>{{ item.content }}</h3>
+                <ul class="pics"><li><img src="http://800cx2.com1.z0.glb.clouddn.com/apicloud/e060fb490183f78e1a66e5046155b1dd.jpeg"/></li></ul>
                 <div>出自：{{ item.origin || 'lin' }}</div>
             </li>
         </ul>
