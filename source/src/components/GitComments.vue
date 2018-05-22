71bccca14095ad9f96a6c6c27f61c700dd4f5901
zhangjialin <zhangjl@xiaomodo.com>
2018-05-22 19:31:27 +0800

    修改git comment 的repo

--- source/src/components/GitComments.vue
+++ source/src/components/GitComments.vue
@@ -10,13 +10,13 @@
 <script>
     import Gitment from '@/libs/gitment.browser'
     export default {
-        name: 'git-comments-container',
+        name: 'git-comments',
         mounted () {
             const that = this
             that.$nextTick(() => {
                 let gitment = new Gitment({
                     owner: 'bravelin',
-                    repo: 'BlogComment',
+                    repo: 'bravelin.github.io',
                     oauth: {
                         client_id: '4f6b659459034ca49ca2',
                         client_secret: '32edded2eaece2226cf18e1f1462d405847594ff'
