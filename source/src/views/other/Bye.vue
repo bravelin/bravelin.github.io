98298bd0da6de3b42a23ccad6ccd9be69e42ddcb
zhangjialin <jialin2013@qq.com>
2018-05-31 21:13:37 +0800

    修改错别字

--- source/src/views/other/Bye.vue
+++ source/src/views/other/Bye.vue
@@ -5,9 +5,9 @@
             <p>标题是借用娉妮离职时候说的话。三年的时间，成长与进步还是有的，在此与大家说再见，虽然相识的同事已经不多了。</p>
             <h3 class="title">关于公司</h3>
             <p>那时候还没有xmd，名称叫shidou，logo挺好看的~</p>
-            <figure class="common"><img style="max-width:500px" src="http://800cx2.com1.z0.glb.clouddn.com/apicloud/a932cbf582c094ba1400114a3e71bd98.png"></figure>  
+            <figure class="common"><img style="max-width:500px" src="http://800cx2.com1.z0.glb.clouddn.com/apicloud/a932cbf582c094ba1400114a3e71bd98.png"></figure>
             <p>刚来的时候技术比较差，也就JavaScript的基础好一点~</p>
-            <p>做shidou的官网弄了好久，被秋建天天催，挺失败的~</p>
+            <p>做shidou的官网弄了好久，被秋建天天催，感觉挺失败的~</p>
             <figure class="common"><img style="max-width:500px" src="http://800cx2.com1.z0.glb.clouddn.com/apicloud/5f237f4ca33746e872de71f66a7b9548.png"></figure>
             <p>公司周边的环境挺好，天气好的时候，可以在操场跑步~</p>
             <h3 class="title">关于项目</h3>
@@ -22,12 +22,12 @@
             <figure class="common"><img style="max-width:400px" src="http://800cx2.com1.z0.glb.clouddn.com/apicloud/3cf4b4c5a5b53d9b9acb2142770c4c64.jpg"/></figure>
             <p>还有那个时候的“个人中心”：</p>
             <figure class="common"><img style="max-width:400px" src="http://800cx2.com1.z0.glb.clouddn.com/apicloud/c85ee8c87bc8224fc707d32bf11444c8.jpg"/></figure>
-            <p>以及改了n多次的优惠券页面：</p>
+            <p>以及改了N多次的优惠券页面：</p>
             <figure class="common"><img style="max-width:400px" src="http://800cx2.com1.z0.glb.clouddn.com/apicloud/5563a748dd8469188c81306a4b41f961.jpg"/></figure>
-            <p>三年的时间里，不停地跌代功能，这是下面部分UI设计图：</p>
+            <p>三年的时间里，不停地迭代功能，这是下面部分UI设计图：</p>
             <figure class="common"><img style="max-width:900px" src="http://800cx2.com1.z0.glb.clouddn.com/apicloud/c1bb7299121a8d96dca3ebcc21b001ec.png"/></figure>
             <h3 class="title">关于同事</h3>
-            <p>下面这个是明佳，小摩豆的第一代后台开发工程师，：</p>
+            <p>下面这个是明佳，xmd的第一代后台开发工程师：</p>
             <figure class="common"><img style="max-width:400px" src="http://800cx2.com1.z0.glb.clouddn.com/apicloud/16ca227c9e0872e6ad555a1459e4e1a5.jpg"></figure>
             <p>桌子上是蓝色外壳的windows phone手机，因无法使用微信，做公众号开发的时候只好借我的手机调试，那时候公司连一台像样的测试机都没有[冷汗]~</p>
             <p>明生略显落寞的背影：</p>
@@ -35,7 +35,7 @@
             <p>忘不了在工作中，各位同事给与的鼓励与支持：</p>
             <figure class="common"><img style="max-width:800px" src="http://800cx2.com1.z0.glb.clouddn.com/apicloud/00a05376fbe82279e0588fcfcdecb455.png"/></figure>
             <figure class="common"><img style="max-width:400px" src="http://800cx2.com1.z0.glb.clouddn.com/apicloud/3656599f328b4d2668c0d78ee2271da7.png" /></figure>
-            <p>包括走了的和即将走的绵生、秋建、宏真、益峰、阳亚、建伟、叶总、张荣、明生、鹏辉、彭彬、娉妮、小丁、加灿、明佳......（名单比较长，可能忘了）</p>
+            <p>包括走了的和即将走的绵生、秋建、宏真、益峰、阳亚、建伟、叶总、张荣、明生、鹏辉、彭彬、娉妮、小丁、堃漪、加灿、明佳、程星......（名单比较长，可能忘了）</p>
             <figure class="common"><img style="max-width:400px" src="http://800cx2.com1.z0.glb.clouddn.com/apicloud/0a526b4424548f765c1bd0ef89d29672.png"></figure>
             <p><strong>最后，借用当年明月的话：“成功的方式只有一种，那就是以自己喜欢的方式度过一生”。愿大家此生都能成功！</strong></p>
         </div>
