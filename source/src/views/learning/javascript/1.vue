2ae5134b215ce80201b32cc29c6b549108cb06a5
bravelin <jialin2013@foxmail.com>
2018-06-13 12:12:12 +0800

    add content

--- source/src/views/learning/javascript/1.vue
+++ source/src/views/learning/javascript/1.vue
@@ -3,7 +3,7 @@
         <h2 class="title">你不知道的JavaScript（上卷）笔记</h2>
         <div class="content">
             <h3 class="title">作用域</h3>
-            <p><strong>作用域</strong>是存储变量并如何找寻变量的规则。</p>
+            <p><strong>作用域</strong>是存储变量并如何找寻变量的规则。通常将JavaScript归为动态或解释型语言，但<strong>实际上它是一门编译语言</strong>，只是它不是提前编译，编译结果也不能移植。大部分情况下编译发生在执行前的几微秒，看起来像解释执行一样。</p>
         </div>
         <footer>2018年05月20日</footer>
         <Comments></Comments>
