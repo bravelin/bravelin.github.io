c914a657cc74b3208f15bded087dd3f7f3677392
bravelin <jialin2013@foxmail.com>
2018-07-24 15:45:38 +0800

    add content

--- source/src/views/learning/canvas/5.vue
+++ source/src/views/learning/canvas/5.vue
@@ -134,6 +134,11 @@ function calculateFps () {
     lastTime = now
     return fps
 }</code></pre>
+                <h3 class="title">动画的一些原则</h3>
+                <p><strong>将业务逻辑的更新与动画的绘制分开；</strong></p>
+                <p><strong>使用“基于时间的运动”来协调动画的播放速度；</strong></p>
+                <p><strong>使用剪辑区域或图块复制技术将复杂的背景恢复到屏幕之上；</strong></p>
+                <p><strong>必要时可使用一个或者多个离屏缓冲区以提升绘制速度；</strong></p>
         </div>
         <footer>2016年07月10日</footer>
         <Comments></Comments>
@@ -146,14 +151,15 @@ function calculateFps () {
         extends: Page,
         data () {
             return {
+                lastTime: 0,
                 discs: [
                     {
                         x: 150,
                         y: 80,
                         lastX: 150,
                         lastY: 80,
-                        velocityX: -3.2,
-                        velocityY: 3.5,
+                        velocityX: -32,
+                        velocityY: 35,
                         radius: 20,
                         innerColor: 'rgba(255,255,0,1)',
                         middleColor: 'rgba(255,255,0,0.7)',
@@ -165,8 +171,8 @@ function calculateFps () {
                         y: 150,
                         lastX: 50,
                         lastY: 150,
-                        velocityX: 2.2,
-                        velocityY: 2.5,
+                        velocityX: 22,
+                        velocityY: 25,
                         radius: 20,
                         innerColor: 'rgba(100,145,230,1)',
                         middleColor: 'rgba(100,145,230,0.7)',
@@ -178,8 +184,8 @@ function calculateFps () {
                         y: 75,
                         lastX: 150,
                         lastY: 75,
-                        velocityX: 1.2,
-                        velocityY: 1.5,
+                        velocityX: 12,
+                        velocityY: 15,
                         radius: 20,
                         innerColor: 'rgba(255,0,0,1)',
                         middleColor: 'rgba(255,0,0,0.7)',
@@ -252,16 +258,32 @@ function calculateFps () {
             },
             update () {
                 const that = this
+                let deltX = 0
+                let deltY = 0
+                let time = (+new Date())
+                let elapsedTime = that.lastTime ? (time - that.lastTime) : 0
                 that.discs.forEach(disc => {
-                    if (disc.x + disc.velocityX + disc.radius > that.ctx.canvas.width || disc.x + disc.velocityX - disc.radius < 0) {
+                    deltX = disc.velocityX * (elapsedTime / 1000)
+                    deltY = disc.velocityY * (elapsedTime / 1000)
+                    if (disc.x + deltX + disc.radius > that.ctx.canvas.width || disc.x + deltX - disc.radius < 0) {
                         disc.velocityX = -disc.velocityX
                     }
-                    if (disc.y + disc.velocityY + disc.radius > that.ctx.canvas.height || disc.y + disc.velocityY - disc.radius < 0) {
+                    if (disc.y + deltY + disc.radius > that.ctx.canvas.height || disc.y + deltY - disc.radius < 0) {
                         disc.velocityY = -disc.velocityY
                     }
-                    disc.x += disc.velocityX
-                    disc.y += disc.velocityY
+                    disc.x += deltX
+                    disc.y += deltY
+
+                    // if (disc.x + disc.velocityX + disc.radius > that.ctx.canvas.width || disc.x + disc.velocityX - disc.radius < 0) {
+                    //     disc.velocityX = -disc.velocityX
+                    // }
+                    // if (disc.y + disc.velocityY + disc.radius > that.ctx.canvas.height || disc.y + disc.velocityY - disc.radius < 0) {
+                    //     disc.velocityY = -disc.velocityY
+                    // }
+                    // disc.x += disc.velocityX
+                    // disc.y += disc.velocityY
                 })
+                that.lastTime = time
             }
         }
     }
