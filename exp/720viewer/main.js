f762795a601dbd81891ac4e9c772fcedf0264c36
zhangjialin <jialin2013@qq.com>
2019-03-27 16:19:29 +0800

    修改720全景图例子

--- exp/720viewer/main.js
+++ exp/720viewer/main.js
@@ -1,5 +1,5 @@
 // 全景图地址
-let url = 'http://stdag-image.oss-cn-beijing.aliyuncs.com/camera_one_image/20190327/31384703333230301b002800_1553665597-20190327134716847.jpg'
+let url = './exp/0.jpg'
 
 document.addEventListener('DOMContentLoaded', function () {
     const viewer = new PhotoSphereViewer({
