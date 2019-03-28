0d79a3a314ada9369f998a006cbb045c2672c861
zhangjialin <jialin2013@qq.com>
2019-03-28 09:06:20 +0800

    720viewer增加返回按钮

--- exp/720viewer/main.js
+++ exp/720viewer/main.js
@@ -1,12 +1,60 @@
-// 全景图地址
-let url = './exp/0.jpg'
+const defaultUrl = './exp/0.jpg' // 全景图地址
+
+// 从链接上获取图片的url
+let querys = location.href.split('?')[1]
+const paramsObj = {}
+if (querys) {
+    const queryArr = querys.split('&')
+    if (queryArr) {
+        queryArr.forEach(function (item) {
+            let itemArr = item.split('=')
+            if (itemArr && itemArr.length == 2) {
+                paramsObj[itemArr[0]] = decodeURIComponent(itemArr[1])
+            }
+        })
+    }
+}
+
+// 全景图图片url
+const url = paramsObj.url || defaultUrl
 
 document.addEventListener('DOMContentLoaded', function () {
+    const ua = navigator.userAgent.toLowerCase()
+    if (/miniProgram/i.test(ua)) { // 小程序中
+        initViewer(true)
+    } else {
+        const timer = setTimeout(function () {
+            initViewer(false)
+        }, 1000)
+        wx.miniProgram.getEnv(function (res) { // 非微信浏览器中不会触发回调
+            clearTimeout(timer)
+            initViewer(res.miniprogram)
+        })
+    }
+})
+
+// flag为true表明是在小程序中
+function initViewer (flag) {
+    console.log('in miniprogram....', flag)
+    let navBarOptions
+    if (!flag) {
+        navBarOptions = ['autorotate', 'zoom', 'download', 'markers', 'caption', 'fullscreen']
+    } else {
+        navBarOptions = ['autorotate', 'zoom', 'download', 'markers', {
+            id: 'back-button',
+            title: '返回',
+            className: 'psv-button--hover-scale back-button',
+            content: '<i class="back-icon"></i>',
+            onClick: function () {
+                wx.miniProgram.navigateBack()
+            }
+        }, 'caption', 'fullscreen']
+    }
     const viewer = new PhotoSphereViewer({
         panorama: url,
         container: document.getElementById('app'),
         time_anim: false,
-        navbar: true,
+        navbar: navBarOptions,
         default_fov: 90
     })
-})
+}
